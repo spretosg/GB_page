f982ee81719205f0cee33b5d5e1a0305b1f50475
spretosg <66713886+spretosg@users.noreply.github.com>
2026-03-31 10:45:52 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://glarnerberggeiss.com',
-  base: 'https://glarnerberggeiss.com',
+  base: '/',
   output: 'static'
 });
