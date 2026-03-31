a8909159014fcd7cce6f6d93f693c91ea49a224c
spretosg <reto.spielhofer@nina.no>
2026-03-31 10:35:19 +0200

    config root path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://spretosg.github.io',
-  base: '/GB_page',
+  site: "https://glarnerberggeiss.com",
+  base: "/"
   output: 'static'
 });
